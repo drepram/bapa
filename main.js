197e547bcd2d54ed93417792705ab2834b27a0a0
Andre Pramaditya <caxvis@gmail.com>
2019-07-12 12:26:01 +0700

    Shows current time

--- main.js
+++ main.js
@@ -503,3 +503,21 @@ function dapatkanWaktu () {
   document.getElementById('tanggal').value = `${hari}, ${tanggal} ${bulan} ${tahun}`
   document.getElementById('waktu').value = `${jamDanMenit}`
 }
+
+(function () {
+    function checkTime(i) {
+        return (i < 10) ? "0" + i : i;
+    }
+
+    function startTime() {
+        var today = new Date(),
+            h = checkTime(today.getHours()),
+            m = checkTime(today.getMinutes()),
+            s = checkTime(today.getSeconds());
+        document.querySelector('#waktuPadaSaatIni').innerHTML = h + ":" + m + ":" + s;
+        t = setTimeout(function () {
+            startTime()
+        }, 500);
+    }
+    startTime();
+})();
